1d17569065aaf6e5f6e8827f6390a8ecd802bdc6
AntoineF00 <113095213+ToineF@users.noreply.github.com>
2023-09-19 20:21:05 +0200

    Basic Shoot :fuelpump:

--- Run'n'Cheese/Assets/Scripts/Blobfish/Blobfish.cs
+++ Run'n'Cheese/Assets/Scripts/Blobfish/Blobfish.cs
@@ -5,10 +5,33 @@ using UnityEngine;
 public class Blobfish : MonoBehaviour
 {
 
-    [Tooltip("The layer with which the bullet")][SerializeField] private LayerMask _collidingLayer;
+    [Tooltip("The layer with which the bullet")] [SerializeField] private LayerMask _collidingLayer;
+    private Rigidbody2D _rb;
+    private Vector2 _direction;
+    public Vector2 Direction { set => _direction = value; get => _direction; }
+    [SerializeField] private float _speed;
 
-    //private void OnCollisionEnter(Collision collision)
-    //{
-    //    if (collision)
-    //}
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _rb.MovePosition(transform.position + (Vector3)Direction * _speed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.collider.gameObject.layer == _collidingLayer)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+
+    }
 }
--- Run'n'Cheese/Assets/Scripts/Player/PlayerShoot.cs
+++ Run'n'Cheese/Assets/Scripts/Player/PlayerShoot.cs
@@ -1,31 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class PlayerShoot : MonoBehaviour
 {
+    private PlayerInputs _inputs;
     [Tooltip("The prefab of the bullet")] [SerializeField] private GameObject _prefabBullet;
     [Tooltip("The numbers of bullets available")] [SerializeField] private float _bullets;
     [Tooltip("The time between each shot in seconds")] [SerializeField] private float _reloadTime;
     private float _reloadTimer;
+
+    private void Awake()
+    {
+        _inputs = new PlayerInputs();
+    }
+
+    private void OnEnable()
+    {
+        _inputs.Enable();
+        _inputs.Player.Shoot.performed += OnShoot;
+    }
+
+    private void OnDisable()
+    {
+        _inputs.Disable();
+        _inputs.Player.Shoot.performed -= OnShoot;
+    }
 
     private void Update()
     {
         _reloadTimer -= Time.deltaTime;
     }
 
-    private void OnShoot()
+    private void OnShoot(InputAction.CallbackContext context)
     {
         if (_bullets > 0 && _reloadTimer < 0)
         {
             _reloadTimer = _reloadTime;
             _bullets--;
-            var myBullet = Instantiate(_prefabBullet);
+            GameObject myBullet = Instantiate(_prefabBullet, transform.position, Quaternion.identity);
+            Blobfish myBlobfish = myBullet.GetComponent<Blobfish>();
+            if (myBlobfish != null)
+            {
+                Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector2 mouseDirection = mousePosition - (Vector2)transform.position;
+                myBullet.GetComponent<Blobfish>().Direction = mouseDirection.normalized;
+            }
         }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        
     }
 }
