b33a6646235afea5b5edb18cff7a455ad225d7a9
P3D\antoine.foucault <antoine.foucault777@gmail.com>
2023-09-22 15:14:19 +0200

    Le dernier push d'Hugo

--- Run'n'Cheese/Assets/Scripts/Enemies/Enemy.cs
+++ Run'n'Cheese/Assets/Scripts/Enemies/Enemy.cs
@@ -4,4 +4,11 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<PushZone>() != null)
+            return;
+        Debug.Log(collision.collider.name);
+        Destroy(gameObject);
+    }
 }
