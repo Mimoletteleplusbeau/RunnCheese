10860dd2005b9a072063692a8bb2c2a7bc6a9038
AntoineF00 <113095213+ToineF@users.noreply.github.com>
2023-09-23 14:38:28 +0200

    Crab :seat:

--- Run'n'Cheese/Assets/Scripts/Enemies/Crab.cs
+++ Run'n'Cheese/Assets/Scripts/Enemies/Crab.cs
@@ -7,47 +7,57 @@ public class Crab : Enemy
     [SerializeField] private float _speed;
     private Vector3 _direction;
     private Collider2D _boxCollider;
+    private Rigidbody2D _rb;
+    private RaycastHit2D[] _raycastHits;
 
     [Header("Collisions")]
     [Tooltip("The layer of the platforms the enemy can detect")] [SerializeField] private LayerMask platformsLayerMask;
     [SerializeField] private float _groundedOffset = 0.05f;
-    private float _checkTime;
+    private bool _grounded;
 
     private void Awake()
     {
         _direction = Vector2.right;
         _boxCollider = GetComponent<Collider2D>();
+        _rb = GetComponent<Rigidbody2D>();
+        _raycastHits = new RaycastHit2D[2];
     }
 
     private void Update()
     {
-        transform.position += _direction * _speed;
+        Move();
         CheckForGround();
+    }
+
+    private void Move()
+    {
+        Vector2 targetPosition = transform.position + _direction * _speed;
+        _rb.MovePosition(targetPosition);
     }
 
     private void CheckForGround()
     {
-        _checkTime -= Time.deltaTime;
-        if (_checkTime > 0) return;
-        if (!IsGrounded())
+        Debug.Log(IsGrounded());
+        if (_grounded && !IsGrounded())
         {
             _direction *= -1;
-            _checkTime = 1;
-        } else
+            _grounded = false;
+        }
+        else if (IsGrounded())
         {
-            Debug.Log("grounded");
+            _grounded = true;
         }
     }
 
     private bool IsGrounded()
     {
-        RaycastHit2D raycastHit2D = Physics2D.BoxCast(_boxCollider.bounds.center, _boxCollider.bounds.size, 0f, Vector2.down, _groundedOffset, platformsLayerMask);
+        int results = Physics2D.RaycastNonAlloc(_boxCollider.bounds.center + new Vector3(_boxCollider.bounds.extents.x * _direction.x, 0), Vector2.down, _raycastHits, _boxCollider.bounds.extents.y + _groundedOffset, platformsLayerMask);
 
         Color rayColor = Color.red;
         Debug.DrawRay(_boxCollider.bounds.center + new Vector3(_boxCollider.bounds.extents.x, 0), Vector2.down * (_boxCollider.bounds.extents.y + _groundedOffset), rayColor);
         Debug.DrawRay(_boxCollider.bounds.center - new Vector3(_boxCollider.bounds.extents.x, 0), Vector2.down * (_boxCollider.bounds.extents.y + _groundedOffset), rayColor);
         Debug.DrawRay(_boxCollider.bounds.center - new Vector3(_boxCollider.bounds.extents.x, _boxCollider.bounds.extents.y + _groundedOffset), Vector2.right * (_boxCollider.bounds.extents.x), rayColor);
 
-        return raycastHit2D.collider != null;
+        return results > 0;
     }
 }
