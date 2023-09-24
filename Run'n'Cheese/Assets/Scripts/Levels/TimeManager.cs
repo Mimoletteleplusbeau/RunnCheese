25749f5c79baf45f8345a6104b9c24077eb54142
AntoineF00 <113095213+ToineF@users.noreply.github.com>
2023-09-24 20:24:46 +0200

    Timer stop :ticket:

--- Run'n'Cheese/Assets/Scripts/Levels/TimeManager.cs
+++ Run'n'Cheese/Assets/Scripts/Levels/TimeManager.cs
@@ -7,16 +7,25 @@ public class TimeManager : MonoBehaviour
 {
     public static TimeManager instance;
     private float _timer;
+    private bool _hasTimerStopped;
     [SerializeField] private TMP_Text _timerText;
 
     private void Awake()
     {
         instance = this;
+        Seagull.Instance.OnKill += StopTimer;
     }
 
     private void Update()
     {
+        if (_hasTimerStopped) return;
+
         _timer += Time.deltaTime;
         _timerText.text = Mathf.Floor(_timer).ToString();
+    }
+
+    private void StopTimer()
+    {
+        _hasTimerStopped = true;
     }
 }
