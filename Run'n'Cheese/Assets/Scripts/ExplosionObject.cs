00121b12c8e48c7d3c41b0c922376b1924f5b660
ToineF <113095213+ToineF@users.noreply.github.com>
2023-09-26 09:17:07 +0200

    Plateform Break Fix 🦽

--- Run'n'Cheese/Assets/Scripts/ExplosionObject.cs
+++ Run'n'Cheese/Assets/Scripts/ExplosionObject.cs
@@ -5,74 +5,74 @@ using UnityEngine;
 public class ExplosionObject : MonoBehaviour
 {
 
-    //public GameObject originalObject;
-    //public GameObject fracturedObject;
+    public GameObject originalObject;
+    public GameObject fracturedObject;
 
-    //public float explosionMinForce = 5;
-    //public float explosionMaxForce = 100;
-    //public float explosionForceRadius = 10;
-    //public float fragScaleFactor = 1;
+    public float explosionMinForce = 5;
+    public float explosionMaxForce = 100;
+    public float explosionForceRadius = 10;
+    public float fragScaleFactor = 1;
 
-    //private GameObject fractObj;
+    private GameObject fractObj;
 
-    //void Update() // Input de key pour les tests à remplacer par un test de colison avec le projectile
-    //{
-    //    if (Input.GetKeyDown(keyCode.Space)) // Pas compris l'erreur... --> KeyCode existe pas ? 
-    //    {
-    //        Explode();
-    //    }
-    //    if (Input.GetKeyDown(KeyCode.R))
-    //    {
-    //        Reset();
-    //    }
-    //}
+    void Update() // Input de key pour les tests à remplacer par un test de colison avec le projectile
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            Explode();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reset();
+        }
+    }
 
-    //void Explode()
-    //{
-    //    if (originalObject != null)         // Pas utile si tu fais deja ça dans le script original
-    //    {
-    //        originalObject.SetActive(false); // La même ^
+    void Explode()
+    {
+        if (originalObject != null)         // Pas utile si tu fais deja ça dans le script original
+        {
+            originalObject.SetActive(false); // La même ^
 
-    //        if (fracturedObject != null)
-    //        {
-    //            fractObj = Instantiate(fracturedObject) as GameObject;
+            if (fracturedObject != null)
+            {
+                fracturedObject.SetActive(true);
+                foreach (Transform t in fracturedObject.transform)
+                {
+                    Debug.Log(t.name);
+                    var rb = t.GetComponent<Rigidbody>(); 
 
-    //            foreach (Transform t in fractObj.transform)
-    //            {
-    //                var rb = t.GetComponent<Rigbody>(); // Rigbody n'existe pas et je sais pas comment le corriger 
-                    
-    //                if (rb != null)
-    //                    rb.AddExplosionForce(Random.Range(explosionMinForce, explosionMaxForce), originalObject.transform.position, explosionForceRadius);
+                    if (rb != null)
+                        rb.AddExplosionForce(Random.Range(explosionMinForce, explosionMaxForce), originalObject.transform.position, explosionForceRadius);
 
-    //                StartCoroutine(Shrink(t, 2));
-    //            }
+                    StartCoroutine(Shrink(t, 2));
+                }
 
-    //            Destroy(fractObj, 5);
+                Destroy(fractObj, 5);
 
 
-    //        }
-    //    }
-    //}
+            }
+        }
+    }
 
-    //void Reset() // A supr pour le code après 
-    //{
-    //    Destroy(fractObj);
-    //    originalObject.SetActive(true);
-    //}
+    void Reset() // A supr pour le code après 
+    {
+        Destroy(fractObj);
+        originalObject.SetActive(true);
+    }
 
-    //IEnumerator Shrink (Transform t, float delay) // Vanish des props 
-    //{
-    //    yield return new WaitForSeconds(delay);
+    IEnumerator Shrink(Transform t, float delay) // Vanish des props 
+    {
+        yield return new WaitForSeconds(delay);
 
-    //    Vector3 newScale = t.localScale;
+        Vector3 newScale = t.localScale;
 
-    //    while (newScale.x >= 0)
-    //    {
-    //        newScale -= new Vector3(fragScaleFactor, fragScaleFactor, fragScaleFactor);
+        while (newScale.x >= 0)
+        {
+            newScale -= new Vector3(fragScaleFactor, fragScaleFactor, fragScaleFactor);
 
-    //        t.localScale = newScale;
-    //        yield return new WaitForSeconds(0.05f);
-    //    }
-    //}
- 
+            t.localScale = newScale;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
 }
