8922815faeb2ccc00b5059c892733db66ebb50e6
AntoineF00 <113095213+ToineF@users.noreply.github.com>
2023-09-30 17:36:19 +0200

    Kill Zone Improvement :link:
    + Push Zone improvements

--- Run'n'Cheese/Assets/Scripts/GPE/KillZone.cs
+++ Run'n'Cheese/Assets/Scripts/GPE/KillZone.cs
@@ -4,4 +4,11 @@ using UnityEngine;
 
 public class KillZone : Enemy
 {
+
+    protected override void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.GetComponent<PlayerController>() == null) return;
+
+        Destroy(collider.gameObject);
+    }
 }
